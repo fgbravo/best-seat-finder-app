daa00ba3df38791fd647f620c095cee04311759a
fabricio <fgbravo188@gmail.com>
2020-08-21 17:11:22 -0300

    Configures Stimulus and installs it

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,10 @@ require("@rails/activestorage").start()
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("../controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
